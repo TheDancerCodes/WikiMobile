fe62871a34fcb6303463c81adebbc4072726b058
Roger Taracha <rojtaracha@gmail.com>
2018-07-14 01:35:38 +0300

    [FEATURE] Creating Activities:
    [+] Create Activities in Kotlin & Layouts for each.
    [+] Register each to the manifest.

---
+++ app/src/main/java/com/thedancercodes/wikimobile/activities/ArticleDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.thedancercodes.wikimobile.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.thedancercodes.wikimobile.R
+
+
+class ArticleDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
---
+++ app/src/main/java/com/thedancercodes/wikimobile/activities/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.thedancercodes.wikimobile.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import com.thedancercodes.wikimobile.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Enable viewing of detail page using onClick listener
+        message.setOnClickListener { _ ->
+            startActivity(Intent(this, ArticleDetailActivity::class.java))
+        }
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
