4a8364bb27dbfa2ffbbe49f7fd2304d85c21dba7
Roger Taracha <rojtaracha@gmail.com>
2018-07-14 17:49:39 +0300

    [FEATURE] Using the Bottom Navigation View with Fragments:

--- app/src/main/java/com/thedancercodes/wikimobile/activities/MainActivity.kt
+++ app/src/main/java/com/thedancercodes/wikimobile/activities/MainActivity.kt
@@ -5,37 +5,53 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.thedancercodes.wikimobile.R
+import com.thedancercodes.wikimobile.fragments.ExploreFragment
+import com.thedancercodes.wikimobile.fragments.FavoritesFragment
+import com.thedancercodes.wikimobile.fragments.HistoryFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // Instances of Fragments as properties in our Activity
+    private val exploreFragment: ExploreFragment
+    private val favoritesFragment: FavoritesFragment
+    private val historyFragment: HistoryFragment
+
+    // initialize these Fragments within our Base Constructors Initialization
+    init {
+        exploreFragment = ExploreFragment()
+        favoritesFragment = FavoritesFragment()
+        historyFragment = HistoryFragment()
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+
+        // How we click & switch out which fragment is visible
+        // Kotlin:when == Java:switch
+        when(item.itemId){
+            R.id.navigation_explore -> transaction.replace(R.id.fragment_container, exploreFragment)
+            R.id.navigation_favorites -> transaction.replace(R.id.fragment_container, favoritesFragment)
+            R.id.navigation_history -> transaction.replace(R.id.fragment_container, historyFragment)
         }
-        false
+
+        transaction.commit()
+
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Enable viewing of detail page using onClick listener
-        message.setOnClickListener { _ ->
-            startActivity(Intent(this, ArticleDetailActivity::class.java))
-        }
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        // Initial fragment that is set
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, exploreFragment)
+        transaction.commit()
     }
 }
