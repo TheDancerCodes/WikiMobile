61b4f7802eb08984c04b79ec20df68211eb810a5
Roger Taracha <rojtaracha@gmail.com>
2018-07-14 12:09:14 +0300

    [FEATURE] Updating Views and Layouts:
    [+] Updating Views to use the custom Toolbar.
    [+] Implementing Search in the Search Activity.

--- app/src/main/java/com/thedancercodes/wikimobile/activities/ArticleDetailActivity.kt
+++ app/src/main/java/com/thedancercodes/wikimobile/activities/ArticleDetailActivity.kt
@@ -2,7 +2,9 @@ package com.thedancercodes.wikimobile.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.thedancercodes.wikimobile.R
+import kotlinx.android.synthetic.main.activity_article_detail.*
 
 
 class ArticleDetailActivity : AppCompatActivity() {
@@ -10,5 +12,19 @@ class ArticleDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_detail)
+
+        // This sets the supportActionBar using a v7 toolbar we added in our layout.
+        setSupportActionBar(toolbar)
+
+        // Set back button on the left side of the toolbar next to the title
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // Handles back button press by finishing current activity & going back to previous one
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item!!.itemId == android.R.id.home){
+            finish()
+        }
+        return true
     }
 }
--- app/src/main/java/com/thedancercodes/wikimobile/activities/SearchActivity.kt
+++ app/src/main/java/com/thedancercodes/wikimobile/activities/SearchActivity.kt
@@ -1,13 +1,73 @@
 package com.thedancercodes.wikimobile.activities
 
+import android.app.SearchManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.SearchView
+import android.view.Menu
+import android.view.MenuItem
 import com.thedancercodes.wikimobile.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        // Set the supportActionBar using a v7 toolbar we added in our layout.
+        setSupportActionBar(toolbar)
+
+        // Set back button on the left side of the toolbar next to the title
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == android.R.id.home){
+            finish()
+        }
+        return true
+    }
+
+
+    // Set up to get Search Functionality working.
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        // Inflate menu from menu resource that we have
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        // Get specific item created by its ID
+        val searchItem = menu!!.findItem(R.id.action_search)
+
+        // Get reference to SearchManager Service
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        // Get reference to our SearchView from the actionView we assigned in our menu resource &
+        // set SearchableInfo to the current component
+        val searchView = searchItem!!.actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        // It is focused and expanded immediately we start the Activity
+        searchView.setIconifiedByDefault(false)
+        searchView.requestFocus()
+
+        // OnQueryTextListener object: Helps us handle submissions or when text is changed
+        // in the Search bar
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                // Do the search and update the elements
+
+                println("updated search")
+
+                return false
+            }
+
+            override fun onQueryTextChange(s: String?): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 }
