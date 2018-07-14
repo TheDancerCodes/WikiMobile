92e7472e33c55b3d657eb1d9fecc935546fbfd8d
Roger Taracha <rojtaracha@gmail.com>
2018-07-14 16:13:46 +0300

    [FEATURE] Creating Fragments:
    [+] ExploreFragment, FavoritesFragment & HistoryFragment.

---
+++ app/src/main/java/com/thedancercodes/wikimobile/fragments/FavoritesFragment.kt
@@ -0,0 +1,25 @@
+package com.thedancercodes.wikimobile.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.thedancercodes.wikimobile.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+
+}
